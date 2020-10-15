5ef0a7a76f7065638dab550bbf35173df2d22db2
devjeremyjake <devjeremyjake@gmail.com>
2020-10-15 10:24:44 +0100

    word test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    // Bem Naming Convention 
-    <div className="app">
-      <h1>Hello Clever Programmer... Let's Build the slack clone </h1>
-    {/* Header */}
-    {/* Sidebar */}
-    {/* React-Router -> chat screen */}
-    </div>
-  );
+	return (
+		// Bem Naming Convention
+		<div className="app">
+			<h1>Hello Clever Programmer... Let's Build the slack clone lets go</h1>
+			{/* Header */}
+			{/* Sidebar */}
+			{/* React-Router -> chat screen */}
+		</div>
+	);
 }
 
 export default App;
