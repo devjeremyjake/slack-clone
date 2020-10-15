b7b86af8ac6c7e8120c064f8a63105a7ca70d1df
devjeremyjake <devjeremyjake@gmail.com>
2020-10-15 10:18:55 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // Bem Naming Convention 
+    <div className="app">
+      <h1>Hello Clever Programmer... Let's Build the slack clone </h1>
+    {/* Header */}
+    {/* Sidebar */}
+    {/* React-Router -> chat screen */}
+    </div>
+  );
+}
+
+export default App;
